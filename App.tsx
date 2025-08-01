bebf1518a1c77e2544c86ac193125fb983e35bce
gmmamunh <gmmamun3004@gmail.com>
2025-08-01 21:04:28 +0600

    "c"

--- App.tsx
+++ App.tsx
@@ -1,28 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createDrawerNavigator, DrawerNavigationProp } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+type RootDrawerParamList = {
+  Home: undefined;
+  About: undefined;
+  Contact: undefined;
+};
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+type HomeScreenNavigationProp = DrawerNavigationProp<RootDrawerParamList, 'Home'>;
 
+function HomeScreen({ navigation }: { navigation: HomeScreenNavigationProp }) {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        onPress={() => navigation.navigate('About')}
+        title="Go to About"
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+function AboutScreen({ navigation }: { navigation: DrawerNavigationProp<RootDrawerParamList, 'About'> }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+       <Text>About Screen</Text>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
 
-export default App;
+function ContactScreen({ navigation }: { navigation: DrawerNavigationProp<RootDrawerParamList, 'Contact'> }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Contact Screen</Text>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator<RootDrawerParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="About" component={AboutScreen} />
+        <Drawer.Screen name="Contact" component={ContactScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
